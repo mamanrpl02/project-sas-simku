7bcd186117119d5c3d0123e480459a3c6b83c754
mamanrpl02 <abdurrahmanhidayatrpl@gmail.com>
2024-12-20 15:29:07 +0700

    Widget Dashboard

--- app/Filament/Widgets/MyWidget.php
+++ app/Filament/Widgets/MyWidget.php
@@ -4,6 +4,8 @@ namespace App\Filament\Widgets;
 
 use App\Models\DebitTabungan;
 use App\Models\KreditTabungan;
+use App\Models\PemasukanKas;
+use App\Models\PengeluaranKas;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -12,8 +14,8 @@ class MyWidget extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Debit Keseluruhan', 'Rp ' . number_format(DebitTabungan::sum('nominal'), 0, ',', '.')),
-            Stat::make('Total Kredit Keseluruhan', 'Rp ' . number_format(KreditTabungan::sum('nominal'), 0, ',', '.')),
+            Stat::make('Total Debit', 'Rp ' . number_format(DebitTabungan::sum('nominal'), 0, ',', '.')),
+            Stat::make('Total Kredit', 'Rp ' . number_format(KreditTabungan::sum('nominal'), 0, ',', '.')),
             Stat::make('Total Saldo Keseluruhan', function () {
                 // Hitung total debit
                 $totalDebit = DebitTabungan::sum('nominal');
@@ -23,6 +25,20 @@ class MyWidget extends BaseWidget
 
                 // Hitung saldo keseluruhan
                 $totalSaldo = $totalDebit - $totalKredit;
+
+                return 'Rp ' . number_format($totalSaldo, 0, ',', '.');
+            }),
+            Stat::make('Total Pemasukan Kas', 'Rp ' . number_format(PemasukanKas::sum('nominal'), 0, ',', '.')),
+            Stat::make('Total Pengeluaran Kas', 'Rp ' . number_format(PengeluaranKas::sum('nominal'), 0, ',', '.')),
+            Stat::make('Total Saldo Keseluruhan', function () {
+                // Hitung total debit
+                $pemasukan = PemasukanKas::sum('nominal');
+
+                // Hitung total kredit
+                $pengeluaran = PengeluaranKas::sum('nominal');
+
+                // Hitung saldo keseluruhan
+                $totalSaldo = $pemasukan - $pengeluaran;
 
                 return 'Rp ' . number_format($totalSaldo, 0, ',', '.');
             }),
