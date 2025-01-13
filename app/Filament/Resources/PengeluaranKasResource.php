6f23c58d7da6811dd5ec427fa63d52284e4dfaeb
mamanrpl02 <abdurrahmanhidayatrpl@gmail.com>
2025-01-13 11:00:54 +0700

    add delete

--- app/Filament/Resources/PengeluaranKasResource.php
+++ app/Filament/Resources/PengeluaranKasResource.php
@@ -86,6 +86,8 @@ class PengeluaranKasResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
